dc6f3eee0e9f9aaa89ba90887f80deb9288777ec
Miftahadin Mohamed <miftahadinmohamed99@gmail.com>
2026-07-21 13:35:15 +0300

    Create responsive navbar with React Router links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">MyBlog</h2>
+
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/blogs">Blogs</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
