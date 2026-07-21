8c12f9887dadb18b98f0dd263240be4566a80968
Miftahadin Mohamed <miftahadinmohamed99@gmail.com>
2026-07-21 18:27:35 +0300

    Implement blog listing and dynamic blog details pages

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,15 +1,19 @@
+import { Link } from "react-router-dom";
 import "../styles/BlogCard.css";
+
 function BlogCard({ post }) {
   return (
-    <div className="blog-card">
-      <h2>{post.title}</h2>
+    <Link to={`/blog/${post.id}`} className="blog-link">
+      <div className="blog-card">
+        <h2>{post.title}</h2>
 
-      <p>{post.description}</p>
+        <p>{post.description}</p>
 
-      <span>{post.category}</span>
+        <span>{post.category}</span>
 
-      <p>By {post.author}</p>
-    </div>
+        <p>By {post.author}</p>
+      </div>
+    </Link>
   );
 }
 
--- src/pages/BlogDetails.jsx
+++ src/pages/BlogDetails.jsx
@@ -1,7 +1,20 @@
+import { useParams } from "react-router-dom";
+import posts from "../data/posts";
+
 function BlogDetails() {
+  const { id } = useParams();
+
+  const post = posts.find((post) => post.id === Number(id));
+
   return (
     <div>
-      <h1>Blog Details Page</h1>
+      <h1>{post.title}</h1>
+
+      <p>{post.description}</p>
+
+      <p>Category: {post.category}</p>
+
+      <p>By {post.author}</p>
     </div>
   );
 }
